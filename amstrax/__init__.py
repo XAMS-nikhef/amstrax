50e69dfd021f1c427e4f994306bac65427aebf47
Peter Gaemers <petergaemers@hotmail.com>
2020-11-09 16:02:03 +0100

    fix daq reader

--- amstrax/__init__.py
+++ amstrax/__init__.py
@@ -7,6 +7,9 @@ __version__ = '0.0.1'
 from . import daqreader
 from .daqreader import *
 
+from . import pax_interface
+from .pax_interface import *
+
 from . import peak_processing
 from .peak_processing import *
 
