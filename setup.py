0c0e94b4870d780ec69edd988e1035bd5e2f9742
Joran Angevaare <jorana@nikhef.nl>
2021-12-01 17:37:06 +0100

    make package mature

--- setup.py
+++ setup.py
@@ -1,10 +1,42 @@
 import setuptools
 
+# Get requirements from requirements.txt, stripping the version tags
+with open('requirements.txt') as f:
+    requires = [
+        r.split('/')[-1] if r.startswith('git+') else r
+        for r in f.read().splitlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
+with open('HISTORY.md') as file:
+    history = file.read()
+
 setuptools.setup(name='amstrax',
                  version='0.0.1',
-                 description='XAMS on strax',
-                 install_requires=['strax'],
+                 description='strax for XAMS data',
+                 author='Nikhef',
+                 url='https://github.com/XAMS-nikhef/amstrax',
+                 long_description=readme + '\n\n' + history,
+                 long_description_content_type="text/markdown",
+                 setup_requires=['pytest-runner'],
+                 install_requires=requires,
+                 tests_require=requires + [
+                     'pytest',
+                     'hypothesis',
+                     'boltons',
+                     'flake8',
+                 ],
                  python_requires=">=3.6",
                  packages=setuptools.find_packages(),
-                 zip_safe = False)
-
+                 scripts=[],
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: BSD License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.6',
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: Implementation :: CPython',
+                     'Topic :: Scientific/Engineering :: Physics',
+                 ],
+                 zip_safe=False)
