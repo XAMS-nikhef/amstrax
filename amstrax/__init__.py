374677ea7f9ba9553ca6dd62e317e338afaff51d
Joran Angevaare <jorana@nikhef.nl>
2021-12-02 09:31:00 +0100

    restructure plugins

--- amstrax/__init__.py
+++ amstrax/__init__.py
@@ -1,25 +1,5 @@
 __version__ = '0.1.0'
 
-from . import daqreader
-from .daqreader import *
-
-from . import pax_interface
-from .pax_interface import *
-
-from . import peak_processing
-from .peak_processing import *
-
-from . import pulse_processing
-from .pulse_processing import *
-
-from . import pulse_processing_alt_baseline
-from .pulse_processing_alt_baseline import *
-
-from . import peak_processing
-from .peak_processing import *
-
-from . import event_processing
-from .event_processing import *
 
 from . import hitfinder_thresholds
 from .hitfinder_thresholds import *
@@ -29,3 +9,6 @@ from .itp_map import *
 from .rundb import *
 from .contexts import *
 from .hitfinder_thresholds import *
+
+from . import plugins
+from .plugins import *
---
+++ amstrax/plugins/__init__.py
@@ -0,0 +1,20 @@
+from . import daqreader
+from .daqreader import *
+
+from . import peak_processing
+from .peak_processing import *
+
+from . import pulse_processing
+from .pulse_processing import *
+
+from . import pulse_processing_alt_baseline
+from .pulse_processing_alt_baseline import *
+
+from . import peak_processing
+from .peak_processing import *
+
+from . import event_processing
+from .event_processing import *
+
+from . import pax_interface
+from .pax_interface import *
