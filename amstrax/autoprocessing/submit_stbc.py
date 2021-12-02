0f03a49f4a3bf306071e3b8381bbc1b46cf20fb6
Joran Angevaare <jorana@nikhef.nl>
2021-12-02 14:16:57 +0100

    add main

--- amstrax/autoprocessing/submit_stbc.py
+++ amstrax/autoprocessing/submit_stbc.py
@@ -1,4 +1,6 @@
 import os
+import argparse
+
 
 script_template = """#!/bin/bash
 export PATH=/data/xenon/joranang/anaconda/bin:$PATH
@@ -9,17 +11,17 @@ echo "Script complete, bye!"
 """
 
 
-def submit_job(run_name, target, job_folder='./jobs', log_folder = './logs'):
+def submit_job(run_id, target, job_folder='./jobs', log_folder ='./logs'):
     for folder in (job_folder, log_folder):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
     # Build a script to submit to stoomboot cluster
     script_name = os.path.join(job_folder,
-                               f'p_{run_name}_{target}.sh')
-    log_file = os.path.join(log_folder, f'p_{run_name}_{target}.log')
+                               f'p_{run_id}_{target}.sh')
+    log_file = os.path.join(log_folder, f'p_{run_id}_{target}.log')
 
-    arguments = f'{run_name} --target {target}'
+    arguments = f'{run_id} --target {target}'
 
     script_file = open(script_name, 'w')
     script_file_content = script_template.format(
@@ -31,4 +33,24 @@ def submit_job(run_name, target, job_folder='./jobs', log_folder = './logs'):
 
     # Submit the job
     os.system('qsub %s' % script_name)
-    print(f'Submitted job for run {run_name}:{target}')
+    print(f'Submitted job for run {run_id}:{target}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Submit single run',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--run_id',
+        type=str,
+        help="Name of context to use")
+    parser.add_argument(
+        '--target',
+        default='raw_records',
+        help="Name of context to use")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    submit_job(run_id=f'{int(args.run_id):06}', target=args.target)
