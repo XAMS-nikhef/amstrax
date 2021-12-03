317a9cbe69222cc47c44585f5b8f83a941be3184
Joran Angevaare <jorana@nikhef.nl>
2021-12-03 10:43:17 +0100

    fix circular imports

--- amstrax/__init__.py
+++ amstrax/__init__.py
@@ -1,12 +1,12 @@
 __version__ = '0.1.0'
 
-from . import auto_processing
 from . import hitfinder_thresholds
 from . import plugins
 from .common import *
-from .contexts import *
-from .hitfinder_thresholds import *
 from .hitfinder_thresholds import *
 from .itp_map import *
 from .plugins import *
 from .rundb import *
+from .contexts import *
+
+from . import auto_processing
