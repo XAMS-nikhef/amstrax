314333fe919c417f9d983842050951c02622cade
Joran Angevaare <joranangevaare@gmail.com>
2021-12-23 11:45:42 +0100

    Fix docs (#68)
    * Fix docs
    
    * Update readthedocs.yaml
    
    * Update readthedocs.yaml
    
    * config file
    
    * locate file
    
    * Update requirements.txt
    
    * Update .readthedocs.yaml
    
    * Update .readthedocs.yaml
    
    * Update requirements.txt
    
    * cleaner
    
    * try again
    
    * try again
    
    * retry
    
    * setup should be complete
    
    * fix
    
    * last fix

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 import setuptools
 
-# Get requirements from requirements.txt, stripping the version tags
-with open('requirements.txt') as f:
-    requires = [
-        r.split('/')[-1] if r.startswith('git+') else r
-        for r in f.read().splitlines()]
+
+def open_requirements(path):
+    with open(path) as f:
+        requires = [
+            r.split('/')[-1] if r.startswith('git+') else r
+            for r in f.read().splitlines()]
+    return requires
+
+
+with open('README.md') as file:
+    readme = file.read()
+
+
+with open('HISTORY.md') as file:
+    history = file.read()
+
+requires = open_requirements('requirements.txt')
+tests_requires = open_requirements('extra_requirements/requirements-tests.txt')
+doc_requires = open_requirements('extra_requirements/requirements-docs.txt')
 
 with open('README.md') as file:
     readme = file.read()
@@ -21,12 +35,10 @@ setuptools.setup(name='amstrax',
                  long_description_content_type="text/markdown",
                  setup_requires=['pytest-runner'],
                  install_requires=requires,
-                 tests_require=requires + [
-                     'pytest',
-                     'hypothesis',
-                     'boltons',
-                     'flake8',
-                 ],
+                 extras_require={
+                     'docs': doc_requires,
+                 },
+                 tests_require=tests_requires,
                  python_requires=">=3.6",
                  packages=setuptools.find_packages(),
                  scripts=[],
@@ -35,8 +47,12 @@ setuptools.setup(name='amstrax',
                      'License :: OSI Approved :: BSD License',
                      'Natural Language :: English',
                      'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
                      'Intended Audience :: Science/Research',
                      'Programming Language :: Python :: Implementation :: CPython',
                      'Topic :: Scientific/Engineering :: Physics',
                  ],
-                 zip_safe=False)
+                 zip_safe=False,
+                 )
