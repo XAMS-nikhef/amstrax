d07c4a6045f93aff246ce3e16935569087aa4559
Maricke Flierman <53177596+mflierm@users.noreply.github.com>
2021-12-23 14:39:24 +0100

    Update HISTORY.md with new release (#70)
    Update HISTORY.md with new release (including copy_live script #51 , among other things (see release v.1.1.2 and commits below)).
    
    * Script that automatically copies new data to stoomboot
    
    Script that automatically copies new data to stoomboot.
    
    * move file
    
    * small refactor
    
    * Removed max_runs and raise error for invalid detector
    
    Removed max_runs variable and raise error if invalid detector is used. Later I want to soft-code (is that a word?) the database collections. Furthermore I want to find a way in which we see the TypeError appearing in the webinterface.
    
    * Implemented new for-loop
    
    Implemented new for-loop to save poor DAQ machine and query through the database just once.
    
    * Oops, saw some other tiny mistakes but fixed them
    
    * Update auto_processing.py
    
    * Update amstrax/auto_processing/copy_live.py
    
    Co-authored-by: Joran Angevaare <joranangevaare@gmail.com>
    
    * Small change (check host location and run number format)
    
    * Inplemented logger, move command and config file usage!
    This will make our lives much much easier also for runs collection.
    We probably don't need the consoleHandler. Next up:
    implementing Joran's main loop.
    
    * Tiny change
    
    * Made separate functions for argsparser, configparser and logs. Also the infinite loop is working reasonably. The only problem is that every time it restarts, it seems to be running main() round_nr * 2 times?
    
    * Final version (?) with working log function and iteration
    
    * final tweaks (#52)
    
    Everything works as it should with these fancy readability upgrades. Time to merge!
    
    * For copy_live: update runsdb also with {"processing_status": "pending"} after succesfull copying
    
    * Changed autoprocessing to one with config file
    Small layout thing submit_stbc
    
    * Small layout change for submit_stbc
    
    * Update requirements.txt
    
    * Create test_basics.py
    
    * Update requirements.txt
    
    * fix codefactor
    
    * fix bandit file
    
    * Revert "Changed autoprocessing to one with config file"
    
    This reverts commit aeafdc5930177a97a76defb99e6266cc50a356fd.
    
    * Update submit_stbc.py
    
    * remove notebooks
    
    * Update HISTORY.md
    
    * Update HISTORY.md
    
    * Bump version: 1.0.2 → 1.1.0
    
    * Bump version: 1.1.0 → 1.1.1
    
    * Removed amstrax_notebooks reference from this folder
    
    Co-authored-by: Joran Angevaare <jorana@nikhef.nl>
    Co-authored-by: Joran Angevaare <joranangevaare@gmail.com>
    Co-authored-by: xams-daq <xamsemergency@gmail.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='amstrax',
-                 version='1.0.2',
+                 version='1.1.1',
                  description='strax for XAMS data',
                  author='Nikhef',
                  url='https://github.com/XAMS-nikhef/amstrax',
