ae3a7747ef33752cdd46bc3471919ef191589796
Serena Di Pede <48323986+dipedeserena@users.noreply.github.com>
2022-01-17 19:03:26 +0100

    Update the submit job script  (#104)
    * Update submit_stbc.py
    
    In this commit:
     - change the directory path where to save the staged process data
     - fix job and log folder 
     - change the qsub submission command
    
    * Update submit_stbc.py
    
    add context and the detector specs for submitting processing
    
    * Update auto_processing.py
    
    fix help
    
    * Added new submit_job variables and arguments also in auto_processing
    
    * Added 'detector' and 'context' argument as used by submit_stbc.py
    
    * Made --detector flag explicit, since otherwise it was complaining in the script_template
    
    * Removed 'script' argument in submit_job function, since we want it to be amstraxer by default
    
    * Update submit_stbc.py
    
    1. export the path again and source the environment 
    2. Add a little docstring for the path where data are saved
    
    * Update auto_processing.py
    
    For now, change back to "process_run.py" the default script for submitting job for auto processing
    
    Co-authored-by: mflierm <m.flierman@nikhef.nl>

--- amstrax/auto_processing/submit_stbc.py
+++ amstrax/auto_processing/submit_stbc.py
@@ -6,20 +6,25 @@ from amstrax import amstrax_dir
 script_template = """#!/bin/bash
 export PATH=/project/xenon/jorana/software/miniconda3/bin:$PATH
 source activate /data/xenon/joranang/anaconda/envs/amstrax_2021
-cd /data/xenon/xamsl/processing_stage
+cd /data/xenon/{detector}/processing_staged_run
 echo "starting script!"
 which python
-python {amstrax_dir}/auto_processing/{script}.py {arguments}
+python {amstrax_dir}/auto_processing/{script}.py {arguments} --detector {detector}
 echo "Script complete, bye!"
 """
 
-
 def submit_job(run_id, 
                target,
-               job_folder='./jobs', 
-               log_folder='./logs',
+               context,
+               detector,
+               job_folder='jobs', 
+               log_folder='logs',
                script = 'amstraxer'
               ):
+    """
+    This script will save data to
+    /data/xenon/{detector}/processing_staged_run/amstrax_data
+    """
     for folder in (job_folder, log_folder):
         if not os.path.exists(folder):
             os.makedirs(folder)
@@ -27,16 +32,16 @@ def submit_job(run_id,
     # Build a script to submit to stoomboot cluster
     script_name = os.path.join(os.path.abspath('.'),
                                job_folder,
-                               f'p_{run_id}_{target}.sh')
+                               f'p_{run_id}_{target}_{context}.sh')
     log_file = os.path.join(os.path.abspath('.'),
                             log_folder,
-                            f'p_{run_id}_{target}.log')
+                            f'p_{run_id}_{target}_{context}.log')
 
-    arguments = f' {run_id} --target {target}'
-
+    arguments = f' {run_id} --target {target} --context {context}'
     script_file = open(script_name, 'w')
     script_file_content = script_template.format(
         arguments=arguments,
+        detector=detector,
         amstrax_dir=amstrax_dir,
         script=script,
     )
@@ -44,11 +49,10 @@ def submit_job(run_id,
     script_file.close()
 
     # Submit the job
-    command = f'qsub {script_name} -e {log_file} -o {log_file}'
+    command = f'qsub {script_name} -j oe -o {log_file}'
     print(command)
     os.system(command)
-    print(f'Submitted job for run {run_id}:{target}')
-
+    print(f'Submitted job for run {run_id}:{target}:{context}')
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -57,14 +61,22 @@ def parse_args():
     parser.add_argument(
         '--run_id',
         type=str,
-        help="Name of context to use")
+        help="ID of the run to process; usually the run name.")
     parser.add_argument(
         '--target',
-        default='raw_records',
-        help="Name of context to use")
+        default='raw_records_v1724',
+        help="Target final data type to produce.")
+    parser.add_argument(
+        '--context',
+        default='xams_little',
+        help="xams_little or xams")
+    parser.add_argument(
+        '--detector',
+        default='xamsl',
+        help="xamsl or xams")
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    submit_job(run_id=f'{int(args.run_id):06}', target=args.target)
+    submit_job(run_id=f'{int(args.run_id):06}', target=args.target, context=args.context, detector=args.detector)
