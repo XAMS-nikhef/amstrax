17e0fee6aa904f5b826a92ab7352eccc61adef5e
mflierm <m.flierman@nikhef.nl>
2022-01-17 16:16:12 +0100

    Added new submit_job variables and arguments also in auto_processing

--- amstrax/auto_processing/auto_processing.py
+++ amstrax/auto_processing/auto_processing.py
@@ -20,6 +20,14 @@ def parse_args():
         default=None,
         type=int,
         help="Max number of jobs to submit, if you exceed this number, break submitting new jobs")
+    parser.add_argument(
+        '--context',
+        default='xams_little',
+        help="xams_little or xams")
+    parser.add_argument(
+        '--detector',
+        default='xamsl',
+        help="xamsl or xams")
     return parser.parse_args()
 
 
@@ -35,6 +43,9 @@ if __name__ == '__main__':
     # settings
     nap_time = int(args.timeout)
     max_jobs = int(args.max_jobs) if args.max_jobs is not None else None
+    context = args.context
+    detector = args.detector
+    target = args.target
     runs_col = amstrax.get_mongo_collection()
 
     while 1:
@@ -47,7 +58,7 @@ if __name__ == '__main__':
 
         for run_doc in run_docs_to_do[:max_jobs]:
             run_name = f'{int(run_doc["number"]):06}'
-            submit_stbc.submit_job(run_name, target='raw_records_aqmon', script='process_run')
+            submit_stbc.submit_job(run_name, target=target, script='process_run', context=context, detector=detector)
             runs_col.find_one_and_update({'number': run_name},
                                          {'$set': {'processing_status': 'submitted_job'
                                                   }})
