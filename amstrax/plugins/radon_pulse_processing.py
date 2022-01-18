dfa3e09697d87f08d728de3ae061103c44c5bf72
Serena Di Pede <48323986+dipedeserena@users.noreply.github.com>
2022-01-18 16:01:01 +0100

    Update and rename pulse_processing.py to radon_pulse_processing.py

--- amstrax/plugins/radon_pulse_processing.py
+++ amstrax/plugins/radon_pulse_processing.py
@@ -15,43 +15,55 @@ HITFINDER_OPTIONS = tuple([
         default='pmt_commissioning_initial',
         help='Minimum hit amplitude in ADC counts above baseline. '
              'See straxen.hit_min_amplitude for options.'
-    )])
+    ),
+    strax.Option(
+        'hit_threshold',
+        default=10,
+        help='Hitfinder threshold in ADC counts above baseline'
+        )
+    ])
 
 
 @export
 @strax.takes_config(
     strax.Option(
         'baseline_samples',
-        default=40,
+        default=40, infer_type=False,
         help='Number of samples to use at the start of the pulse to determine '
              'the baseline'),
     # PMT pulse processing options
     strax.Option(
+        'pmt_pulse_filter',
+        default=None, infer_type=False,
+        help='Linear filter to apply to pulses, will be normalized.'),   
+    strax.Option(
         'save_outside_hits',
-        default=(3, 20),
+        default=(3, 20), infer_type=False,
         help='Save (left, right) samples besides hits; cut the rest'),
     strax.Option(
         'n_tpc_pmts', type=int,
         help='Number of TPC PMTs'),
     strax.Option(
         'check_raw_record_overlaps',
-        default=False, track=False,
+        default=False, track=False, infer_type=False,
         help='Crash if any of the pulses in raw_records overlap with others '
              'in the same channel'),
     strax.Option(
         'allow_sloppy_chunking',
-        default=True, track=False,
+        default=True, track=False, infer_type=False,
         help=('Use a default baseline for incorrectly chunked fragments. '
               'This is a kludge for improperly converted XENON1T data.')),
     *HITFINDER_OPTIONS)
-class PulseProcessing(strax.Plugin):
+class RadonPulseProcessing(strax.Plugin):
     """
-    1. Split raw_records into:
-     - (tpc) records
-     - pulse_counts
-    For TPC records, apply basic processing:
-        1. Flip, baseline, and integrate the waveform
-        3. Find hits, and zero outside hits.
+    Get the specif raw_records of the measurements 
+    (raw_records_v1724 or raw_records_v1730)
+    and split the raw_records into:
+    - radon_records
+    - radon_pulse_counts
+    Apply basic pulse processing:
+        1. Flip the pulse, calculate the baseline and integrate the waveform
+        2. Find hits, and zero outside hits.
 
     pulse_counts holds some average information for the individual PMT
     channels for each chunk of raw_records. This includes e.g.
@@ -59,33 +71,48 @@ class PulseProcessing(strax.Plugin):
     overlap with any other pulse), or mean values of baseline and
     baseline rms channel.
     """
-    __version__ = '0.2.12'
-    # save_when = strax.SaveWhen.NEVER
+    __version__ = '0.2.29'
+    
     parallel = 'process'
     rechunk_on_save = immutabledict(
-        records=False,
-        pulse_counts=True)
-    compressor = 'lz4'
+        radon_records=False,
+        radon_pulse_counts=True)
+    compressor = 'zstd'
 
-    depends_on = 'raw_records'
+    depends_on = ('raw_records_v1724','raw_records_v1730')
 
-    provides = ('records', 'pulse_counts')
+    provides = ('radon_records', 'radon_pulse_counts')
     data_kind = {k: k for k in provides}
-
-    def infer_dtype(self):
-        # Get record_length from the plugin making raw_records
+    save_when = strax.SaveWhen.TARGET
+       
+    def infer_dtype(self,):
+        # The record_length is the same for both raw_records_v1724 and raw_records_v1730
+        # therefore, we can just use one of the two
         self.record_length = strax.record_length_from_dtype(
-            self.deps['raw_records'].dtype_for('raw_records'))
+            self.deps['raw_records_v1724'].dtype_for('raw_records_v1724'))
 
         dtype = dict()
         for p in self.provides:
-            if 'records' in p:
+            if 'radon_records' in p:
                 dtype[p] = strax.record_dtype(self.record_length)
-        dtype['pulse_counts'] = pulse_count_dtype(self.config['n_tpc_pmts'])
+        dtype['radon_pulse_counts'] = pulse_count_dtype(self.config['n_tpc_pmts'])
 
         return dtype
 
-    def compute(self, raw_records, start, end):
+    def compute(self, raw_records_v1724, raw_records_v1730, start, end):       
+        ####
+        # Get the raw_records having data
+        ####
+        # Case 1: return True -> v1724 measurement
+        if self.choose_records(raw_records_v1724,raw_records_v1730):
+            raw_records = raw_records_v1724
+        # Case 2: return False ->  v1730 measurement
+        elif not self.choose_records(raw_records_v1724,raw_records_v1730):
+            raw_records = raw_records_v1730
+        # For the sake of the if-else statement    
+        else:
+            pass
+
         if self.config['check_raw_record_overlaps']:
             check_overlaps(raw_records, n_channels=3000)
 
@@ -108,16 +135,23 @@ class PulseProcessing(strax.Plugin):
                        allow_sloppy_chunking=self.config['allow_sloppy_chunking'],
                        flip=True)
 
-        pulse_counts = count_pulses(r, self.config['n_tpc_pmts'])
-        pulse_counts['time'] = start
-        pulse_counts['endtime'] = end
+        strax.integrate(r)       
+
+        radon_pulse_counts = count_pulses(r, self.config['n_tpc_pmts'])
+        radon_pulse_counts['time'] = start
+        radon_pulse_counts['endtime'] = end
 
         if len(r):
             # Find hits
+            # -- before filtering,since this messes with the with the S/N
             hits = strax.find_hits(
-                r,
-                min_amplitude=amstrax.hit_min_amplitude(
+                r, min_amplitude=amstrax.hit_min_amplitude(
                     self.config['hit_min_amplitude']))
+
+            if self.config['pmt_pulse_filter']:
+                # Filter to concentrate the PMT pulses
+                strax.filter_records(
+                    r, np.array(self.config['pmt_pulse_filter']))
 
             le, re = self.config['save_outside_hits']
             r = strax.cut_outside_hits(r, hits,
@@ -127,19 +161,60 @@ class PulseProcessing(strax.Plugin):
             # Probably overkill, but just to be sure...
             strax.zero_out_of_bounds(r)
 
-        strax.integrate(r)
+        return dict(radon_records=r,
+                    radon_pulse_counts=radon_pulse_counts)
 
-        # First 7 entries give a positive area even though sum('data') = 0
-        # Changing their area to 0 before filtering
-        for i in range(0, 7):
-            r[i]['area'] = 0
+    ###
+    # Make one records for future processing
+    ###
 
-        r = r[r['area'] > 0]
-        # r = r[np.average(r['data']) > 0]
+    def choose_records(self, raw_records_v1724, raw_records_v1730):
+        """
+        This function implements a decisional loop over which raw_records to keep.
+        There are for possibilities:
+            1. V1724 measurements 
+                    - data only from raw_records_v1724
+                    - return True
+            2. V1730 measurements
+                    - data only from raw_records_v1730
+                    - return False
+            3. We have data in both raw_records_v1724 and raw_records_v1730
+                    - this is not possible in XAMSL data campaign
+                    - raise ValueError
+            4. No data in both raw_records_v1724 and raw_records_v1730
+                    - this is not possible in XAMSL data campaign
+                    - raise ValueError          
+        For XAMSL measurements details, please have a look:
+            https://github.com/XAMS-nikhef/amstrax_files/tree/master/data
+        """
 
-        return dict(records=r,
-                    pulse_counts=pulse_counts)
-
+        try:
+            # Case v1724 measurement: data only in the raw_records_v1724
+            if (np.any(raw_records_v1724['channel']) and not np.any(raw_records_v1730['channel'])):
+                ret = True
+            # Case v1730 measurement: data only in the raw_records_v1730
+            elif (np.any(raw_records_v1730['channel']) and not np.any(raw_records_v1724['channel'])):
+                ret = False
+            # raw_records_v1730 and raw_records_v1724 both returning data
+            elif (np.any(raw_records_v1730['channel']) and np.any(raw_records_v1724['channel'])):
+                raise ValueError(
+                    f"Bad data! The raw_records are both returning data and"
+                    f"this is not possible for a XAMSL measurement."
+                    f"Check the raw_records data of this measurement"
+                    f"or do not process it.")
+            # no data in both raw_records_v1730 and raw_records_v1724
+            elif (not np.any(raw_records_v1730['channel']) and not np.any(raw_records_v1724['channel'])):
+                raise ValueError(
+                    f"Bad data! The raw_records are both empty and"
+                    f"this is not possible for a XAMSL measurement."
+                    f"Check the raw_records data of this measurement"
+                    f"or do not process it.")               
+            # For the sake of the if-else statement 
+            else:
+                pass 
+        except Exception as e:
+            print(f"We've got a {type(e)} error in choose_records() and {str(e)}.")
+        return ret
 
 ##
 # Pulse counting
@@ -273,3 +348,4 @@ def _check_overlaps(records, last_end):
             return r['channel'], r['time']
         last_end[r['channel']] = strax.endtime(r)
     return -9999, -9999
+
