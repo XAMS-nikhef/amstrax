552c71ad084ec04aa940da67998b9ebe692466f0
Serena Di Pede <48323986+dipedeserena@users.noreply.github.com>
2022-01-18 16:02:21 +0100

    delete pulse_processing

--- amstrax/plugins/__init__.py
+++ amstrax/plugins/__init__.py
@@ -1,6 +1,5 @@
 from . import daqreader
 from . import pax_interface
-from . import pulse_processing
 from . import pulse_processing_alt_baseline
 from . import radon_pulse_processing
 from . import peak_processing
@@ -10,7 +9,6 @@ from . import peak_basics
 from . import event_processing
 from .daqreader import *
 from .pax_interface import *
-from .pulse_processing import *
 from .pulse_processing_alt_baseline import *
 from .radon_pulse_processing import *
 from .peak_processing import *
@@ -18,4 +16,3 @@ from .radon_peaks import *
 from .radon_peak_basics import *
 from .peak_basics import *
 from .event_processing import *
-
