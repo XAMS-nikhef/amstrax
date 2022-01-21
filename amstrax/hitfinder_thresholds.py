11c807b568f06cb2b3c471e678b42062078ee332
Serena Di Pede <48323986+dipedeserena@users.noreply.github.com>
2022-01-21 13:01:03 +0100

    Delete "threshold" as an argument

--- amstrax/hitfinder_thresholds.py
+++ amstrax/hitfinder_thresholds.py
@@ -7,7 +7,7 @@ export, __all__ = strax.exporter()
 
 
 @export
-def hit_min_amplitude(model, threshold = 15, n_tpc_pmts=8):
+def hit_min_amplitude(model, n_tpc_pmts=8):
     """Return hitfinder height threshold to use in processing.
 
     :param model: Model name (str), or int to use a uniform threshold,
@@ -25,13 +25,13 @@ def hit_min_amplitude(model, threshold = 15, n_tpc_pmts=8):
         # ADC thresholds used for XAMSL PMTs
         # (January 20 2022)
         n_tpc_pmts = 4
-        result = threshold * np.ones(n_tpc_pmts, dtype=np.int16)
+        result = 15 * np.ones(n_tpc_pmts, dtype=np.int16)
         return result
 
     if model == 'xams_thresholds':
         # ADC thresholds used for XAMS PMTs
         # (January 20 2022)
-        result = threshold * np.ones(n_tpc_pmts, dtype=np.int16)
+        result = 15 * np.ones(n_tpc_pmts, dtype=np.int16)
         return result
 
     raise ValueError(f"Unknown ADC threshold model {model}")
