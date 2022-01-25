76a95c02752640eeeef92ed8c9c6b1e0ea37c056
Serena Di Pede <48323986+dipedeserena@users.noreply.github.com>
2022-01-25 13:27:19 +0100

    change the default thresholds models

--- amstrax/plugins/radon_pulse_processing.py
+++ amstrax/plugins/radon_pulse_processing.py
@@ -12,7 +12,7 @@ __all__ += ['NO_PULSE_COUNTS']
 HITFINDER_OPTIONS = tuple([
     strax.Option(
         'hit_min_amplitude',
-        default='xamsl_thresholds',
+        default='xams_thresholds',
         help='Minimum hit amplitude in ADC counts above baseline. '
              'See amstrax.hit_min_amplitude in hitfinder_thresholds.py for options.'
     )])
