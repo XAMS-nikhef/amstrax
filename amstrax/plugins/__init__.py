282a1e033bf34c4baaf2ca5a164f7a6553225288
acolijn <cfuselli@nikhef.nl>
2023-01-25 15:06:20 +0100

    added led calibration

--- amstrax/plugins/__init__.py
+++ amstrax/plugins/__init__.py
@@ -6,8 +6,12 @@ from .raw_records import *
 from . import records
 from .records import *
 
+from . import led_cal
+from .led_cal import *
+
 from . import peaks
 from .peaks import *
 
 from . import events
 from .events import *
+
