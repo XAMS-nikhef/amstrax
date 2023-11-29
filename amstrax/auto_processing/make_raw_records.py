be8677fdc702c41e0f005d969311ee9cf3b09295
Carlo Fuselli <cfuselli@nikhef.nl>
2023-11-29 14:56:45 +0100

    allow raw_records from daq

--- amstrax/auto_processing/make_raw_records.py
+++ amstrax/auto_processing/make_raw_records.py
@@ -5,6 +5,20 @@ import os
 import sys
 import strax
 
+HOST_DEFINITIONS = {
+    'daq': {
+        'output_folder': '/home/xams/data/xams_processed',
+        # we will get the live data dir from the runsdb, so this is not used
+        'live_data_dir': 'from_db',
+        'hostname_regex': 'xams'
+    },
+    'stoomboot': {
+        'output_folder': '/data/xenon/xams_v2/xams_raw_records',
+        'live_data_dir': '/data/xenon/xams_v2/live_data',
+        'hostname_regex': 'nikhef.nl'
+    }
+}
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Process a single run with amstrax',
@@ -15,18 +29,10 @@ def parse_args():
         type=str,
         help="ID of the run to process; usually the run name.")
     parser.add_argument(
-        '--output_folder',
-        default='./strax_data',
-        help='Output folder for context')
-    parser.add_argument(
         '--target',
         nargs='+',
         default=['raw_records'],
         help='Target data type(s) to process')
-    parser.add_argument(
-        '--live_data_dir',
-        default='/data/xenon/xams_v2/live_data',
-        help='Live data directory')
     parser.add_argument(
         '--production',
         action='store_true',
@@ -35,11 +41,27 @@ def parse_args():
     return parser.parse_args()
 
 
+def get_host():
+    """
+    Get the host name.
+    """
+    hostname = os.environ.get('HOSTNAME', None)
+    if hostname is None:
+        raise ValueError('Could not determine host name')
+
+    # check in the host definitions if we know this host
+    for host, host_def in HOST_DEFINITIONS.items():
+        if host_def['hostname_regex'] in hostname:
+            return host
+
+    raise ValueError(f'Could not determine host name {hostname} from HOST_DEFINITIONS {HOST_DEFINITIONS}')
+    
+
 def update_processing_status(runsdb, run_id, status, reason=None, production=False):
     """
     Update the processing status of a run in the database.
     """
-    update = {'status': status, 'time': datetime.datetime.now()}
+    update = {'status': status, 'time': datetime.datetime.now(), 'host': get_host()}
     if reason:
         update['reason'] = str(reason)
 
@@ -65,25 +87,30 @@ def add_data_entry(runsdb, run_id, data_type, location, host, by, user, producti
     else:
         print(f'Would add data entry to run {run_id} of type {data_type} with location {location}')
 
-def process_run(args):
+def process_run(args, runsdb):
     """
     Process a single run based on provided arguments.
     """
 
     import amstrax
 
-    run_id = args.run_id
-    output_folder = args.output_folder
-    live_data = args.live_data_dir
+    hostname = get_host()
 
+    run_id = args.run_id
+
+    run_doc = runsdb.find_one({'number': int(run_id)})
+
+    output_folder = HOST_DEFINITIONS[hostname]['output_folder']
+
+    if hostname == 'daq':
+        live_data = run_doc['daq_config']['strax_output_path']
+    else:
+        live_data = HOST_DEFINITIONS[hostname]['live_data_dir']
+    
     st = amstrax.contexts.xams(output_folder=output_folder, init_rundb=False)
     st.storage += [strax.DataDirectory(live_data, readonly=True)]
-    st.set_config({'live_data_dir': live_data})
 
-    run_col = amstrax.get_mongo_collection()
-    run_doc = run_col.find_one({'number': int(run_id)})
-
-    daqst = amstrax.contexts.context_for_daq_reader(st, run_id, 'xams', run_doc=run_doc, check_exists=False)
+    daqst = amstrax.contexts.context_for_daq_reader(st, run_id, run_doc=run_doc, check_exists=False)
 
     for target in args.target:
         print(f'Processing {target}')
@@ -106,7 +133,7 @@ def main(args):
         update_processing_status(runsdb, args.run_id, 'running', production=args.production)
 
         try:
-            process_run(args)  # Process the run
+            process_run(args, runsdb)  # Process the run
 
             update_processing_status(runsdb=runsdb,
                                      run_id=args.run_id,
@@ -118,14 +145,14 @@ def main(args):
                            run_id=args.run_id,
                            data_type='raw_records',
                            location=args.output_folder,
-                           host='stoomboot',
+                           host=get_host(),
                            by='make_raw_records.py',
                            user=os.environ['USER'],
                            production=args.production)
 
         except Exception as e:
             print(f'Processing of run {args.run_id} failed with error {e}')
-            update_processing_status(runsdb, args.run_id, 'failed', reason=e)
+            update_processing_status(runsdb, args.run_id, 'failed', reason=e, production=args.production)
             runsdb.update_one({'number': int(args.run_id)}, {'$inc': {'processing_failed': 1}})
 
         rd = runsdb.find_one({'number': int(args.run_id)})
@@ -133,8 +160,7 @@ def main(args):
     else:
         status = rd["processing_status"]["status"]
         e = f'Run {args.run_id} is not in submitted mode, but in {status}'
-        update_processing_status(runsdb, args.run_id, 'failed', reason=e)
-
+        update_processing_status(runsdb, args.run_id, 'failed', reason=e, production=args.production)
 
 
 if __name__ == '__main__':
