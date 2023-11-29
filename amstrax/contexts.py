4fc3bd16912d2ea365cfb1762628ef619802f06e
Carlo Fuselli <cfuselli@nikhef.nl>
2023-11-29 14:56:06 +0100

    update contexts

--- amstrax/contexts.py
+++ amstrax/contexts.py
@@ -14,7 +14,7 @@ CONFIG = {
     'DEFAULT_COLLECTION': 'runs_gas'
 }
 
-PROCESSED_DATA_FOLDER = '/home/xams/data/xams_processed'
+PROCESSED_DATA_FOLDER = '/data/xenon/xams_v2/xams_raw_records'
 
 COMMON_OPT_XAMS = dict(
     register_all=[],
