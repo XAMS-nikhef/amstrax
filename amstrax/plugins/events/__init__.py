f8b1b2113626ad1da466c0ab9ad5fbf7f62c87ce
cfuselli <cfuselli@nikhef.nl>
2023-12-06 13:06:10 +0100

    forgot init

--- amstrax/plugins/events/__init__.py
+++ amstrax/plugins/events/__init__.py
@@ -4,6 +4,12 @@ from .events import *
 from . import event_basics
 from .event_basics import *
 
+from . import event_positions
+from .event_positions import *
+
+from . import corrected_areas
+from .corrected_areas import *
+
 from . import event_info
 from .event_info import *
 
