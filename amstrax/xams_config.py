ae75f38b38f219212b8daed9c595ee9b583de7bb
cfuselli <cfuselli@nikhef.nl>
2024-10-18 13:09:27 +0200

    no fallback

--- amstrax/xams_config.py
+++ amstrax/xams_config.py
@@ -80,34 +80,32 @@ class XAMSConfig(Config):
 
         return value
 
-    def find_correction_value(self, correction_data, run_id):
-        """
-        Find the correct correction value based on the run_id, considering ranges,
-        fallback values, and handling wildcards.
-        """
-        run_id = run_id.zfill(6)  # Ensure run_id is always 6 digits
-        value = None
+def find_correction_value(self, correction_data, run_id):
+    run_id = run_id.zfill(6)  # Ensure run_id is always 6 digits
+    value = None
 
-        for run_range in correction_data.keys():
-            if "-" in run_range:
-                start_run, end_run = run_range.split("-")
-                start_run = start_run.zfill(6)
-                if end_run == "*":
-                    end_run = "999999"  # Treat * as the highest possible run ID
-                end_run = end_run.zfill(6)
+    for run_range in correction_data.keys():
+        if "-" in run_range:
+            start_run, end_run = run_range.split("-")
+            start_run = start_run.zfill(6)
+            if end_run == "*":
+                # Only allow * for online corrections
+                # check if there is _dev in the filename
+                if "_dev" not in self.name:
+                    raise ValueError(f"Wildcard '*' is only allowed for online corrections")
+                end_run = "999999"  # Treat * as the highest possible run ID
+            end_run = end_run.zfill(6)
 
-                if start_run <= run_id <= end_run:
-                    value = correction_data[run_range]
-                    break
-            else:
-                if run_range.zfill(6) == run_id:
-                    value = correction_data[run_range]
-                    break
+            if start_run <= run_id <= end_run:
+                value = correction_data[run_range]
+                break
+        else:
+            if run_range.zfill(6) == run_id:
+                value = correction_data[run_range]
+                break
 
-        # If no value was found, check for a fallback in the JSON file
-        if value is None:
-            if "fallback" in correction_data:
-                return correction_data["fallback"]  # Use fallback from the JSON file
-            raise ValueError(f"No valid correction found for run_id {run_id} and no fallback provided.")
+    if value is None:
+        raise ValueError(f"No valid correction found for run_id {run_id} and no fallback is allowed.")
 
-        return value
+    return value
+
