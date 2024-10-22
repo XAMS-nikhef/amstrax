6bbf59de56366f5e947e17ce7cbd9c0920de5e95
cfuselli <cfuselli@nikhef.nl>
2024-10-22 11:42:04 +0200

    more imports

--- amstrax/auto_processing_new/__init__.py
+++ amstrax/auto_processing_new/__init__.py
@@ -3,3 +3,12 @@ from .db_utils import *
 
 from . import job_submission
 from .job_submission import *
+
+from . import offline_processing
+from .offline_processing import *
+
+from . import auto_processing
+from .auto_processing import *
+
+from . import processing
+from .processing import *
